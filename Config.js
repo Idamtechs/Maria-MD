c399e02c310391f1951b603c252040a5853ac8ac
Idamtechs <idamtechnology047@gmail.com>
2024-07-25 02:09:10 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,11 +3,11 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "2348168772328"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘TOLUWALASE✘𓆪_✰",
+global.ytname = "YT: DAVIDGAMING"
+global.socialm = "GitHub: idamtechs"
+global.location = "Nigeria"
 
 global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
 
